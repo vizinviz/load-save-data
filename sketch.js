bd3a5fa32db808df250bbdc32270c0c4fd61a698
chrischne <chrischne@gmail.com>
2019-09-05 20:35:41 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,65 +2,69 @@ let data = [];
 let minTemp = 0;
 let maxTemp = 0;
 
+let currentIndex = 0;
+async function setup () {
+	createCanvas(4 * 1920, 1080);
 
-
-async function setup() {
-	createCanvas(4*1920, 1080);
-
-	await d3.csv('temperatur_ch.csv',function(d){
+	await d3.csv('temperatur_ch.csv', function (d) {
 		return {
 			time: +d.time,
 			temp: +d.jan
 		};
-	}).then(function(csv){
+	}).then(function (csv) {
 		data = csv;
 		console.log(data);
 
-		minTemp = d3.min(data,function(d){
+		minTemp = d3.min(data, function (d) {
 			return d.temp;
 		});
-		maxTemp = d3.max(data,function(d){
+		maxTemp = d3.max(data, function (d) {
 			return d.temp;
 		});
 
-		minTemp = -15;
-		maxTemp = 5;
-		console.log(minTemp,maxTemp);
+		// minTemp = -15;
+		// maxTemp = 5;
+		console.log(minTemp, maxTemp);
 	});
 
-	
 
 
-	noLoop();
+
+	frameRate(10);
 }
 
-function draw() {
+function draw () {
 
 	background(255);
-	let w = width/data.length;
+
+	currentIndex = constrain(currentIndex+1,0,data.length);
+	console.log(currentIndex);
+
+
+	let w = width / data.length;
 	let x = 0;
 	colorMode(RGB);
-	let from = color(0, 0, 255);
-	let middle = color(255,255,0);
-let to = color(255,0,0);
+	let from = color('#FFC200');
+	let middle = color(255, 255, 0);
+	let to = color("#FF4B00");
 
-	for (let i = 0; i < data.length; i++) {
-		const d = data[i];	
-		x = i*w;
-		let amt = map(d.temp,minTemp,maxTemp,0,1);
-		let col = 0;
-		if(amt<0.5){
-			col = lerpColor(from, middle, map(amt,0,0.5,0,1));
-		}
-		else {
-			col = lerpColor(middle, to, map(amt,0.5,1,0,1));
-		}
-		console.log(d.temp,maxTemp,amt);
+	for (let i = 0; i < currentIndex; i++) {
+		const d = data[i];
+		x = i * w;
+		let amt = map(d.temp, minTemp, maxTemp, 0, 1);
+		//let col = 0;
+		// if (amt < 0.5) {
+		// 	col = lerpColor(from, middle, map(amt, 0, 0.5, 0, 1));
+		// }
+		// else {
+		// 	col = lerpColor(middle, to, map(amt, 0.5, 1, 0, 1));
+		// }
+		console.log(d.temp, maxTemp, amt);
 		colorMode(RGB);
-		//let col = lerpColor(from, to, amt);
+		let col = lerpColor(from, to, amt);
 		fill(col);
 		noStroke();
-		rect(x,0,w,height);
+		rect(x, 0, w, height);
 	}
 
 }
