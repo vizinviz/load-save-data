6a527327eaf3b2736f8e7a76e3573401ece5b7ce
chrischne <chrischne@gmail.com>
2019-09-08 15:56:16 +0200

    fine tune

--- sketch.js
+++ sketch.js
@@ -3,14 +3,15 @@ let minTemp = 0;
 let maxTemp = 0;
 
 let colors = ['rgb(165,0,38)','rgb(215,48,39)','rgb(244,109,67)','rgb(253,174,97)','rgb(254,224,144)','rgb(255,255,191)','rgb(224,243,248)','rgb(171,217,233)','rgb(116,173,209)','rgb(69,117,180)','rgb(49,54,149)'];
-
+colors = colors.reverse();	
 let currentIndex = 0;
+let ready = false;
 async function setup () {
-	createCanvas(4 * 1920, 1080);
+	createCanvas(4 * 800, 600);
 
 
 	data = await loadData('temperatur_ch_long.csv');
-
+	
 	console.log(data);
 
 	minTemp = d3.min(data, function (d) {
@@ -23,13 +24,19 @@ async function setup () {
 	console.log('temps: ' , minTemp,maxTemp);
 
 	frameRate(30);
+	ready = true;
 }
 
 function draw () {
 
+	if(!ready){
+		background(255,0,0);
+		return;
+	}
 	background(200);
+	
 
-	currentIndex = constrain(currentIndex + 1, 0, data.length);
+	currentIndex = constrain(currentIndex + 1, 0, data.length-1);
 	//console.log(currentIndex);
 
 
@@ -47,12 +54,18 @@ function draw () {
 		//colorMode(RGB);
 		//let col = lerpColor(from, to, amt);
 		//map value to color index
-		var colorIndex = floor(map(d.temperature, -20, 20,0,11));
+		var colorIndex = floor(map(d.temperature, minTemp, maxTemp,0,colors.length-1));
 		var col = colors[colorIndex];
 		fill(col);
 		noStroke();
 		rect(x, 0, w, height);
 	}
+
+	noStroke();
+	fill(0);
+	
+	textSize(40);
+	text(data[currentIndex].time,100,100);
 
 }
 
