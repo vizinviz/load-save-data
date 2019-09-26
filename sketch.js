b2c52d7cb67fe56e546835c8f7d73b519369468c
chrischne <chrischne@gmail.com>
2019-09-26 22:03:11 +0200

    Revert "Revert "Merge branch 'master' of https://github.com/infovishs19/p5-template""
    This reverts commit b021fc5d83c5559f9035cc76100ad50f92f970e0.

--- sketch.js
+++ sketch.js
@@ -1,52 +1,18 @@
 /// <reference path="./libraries/p5.global-mode.d.ts" />
 
-let data = [];
-let minTemp = 0;
-let maxTemp = 0;
-let minYear = 0;
-let maxYear = 0;
 
-let currentIndex = 0;
-let ready = false;
-
-async function setup () {
+function setup () {
 	createCanvas(4 * 800, 600);
-
-	data = await loadData('temperatur.csv');
-	console.log(data);
-
-	minTemp = minprop(data,'mean_temp');
-	maxTemp = maxprop(data,'mean_temp');
-
-	minYear = minprop(data,'year');
-	maxYear = maxprop(data,'year');
-
-	console.log('temperatures: ', minTemp, maxTemp);
-	console.log('years: ', minYear, maxYear);
 
 	frameRate(30);
 
-	ready = true;
 }
 
 function draw () {
 
-	//red backround if not ready
-	if (!ready) {
-		background(255, 0, 0);
-		return;
-	}
-	background(255);
+	background(200);
 
-	for (var i = 0; i < data.length; i++) {
-		let d = data[i];
 
-		let x = map(d.year,minYear,maxYear,0,width);
-		let darkness = map(d.mean_temp,minTemp,maxTemp,255,0);
-		noStroke();
-		fill(darkness);
-		rect(x,0,20,height);
-	}
 }
 
 function keyTyped(){
