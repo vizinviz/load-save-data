20d65b758c99d963172e9d8a8e1d0b63deb8a384
chrischne <chrischne@gmail.com>
2019-10-20 15:55:25 +0200

    replace csv with json

--- sketch.js
+++ sketch.js
@@ -1,14 +1,14 @@
 /// <reference path="./libraries/p5.global-mode.d.ts" />
 
 var data = [];
-async function setup () {
+function preload(){
+	//data = loadJSON();
+}
+
+function setup () {
 	createCanvas(800, 600);
 
-	data = await loadData('temperatur.csv');
-	console.log(data);
-
 	frameRate(30);
-
 }
 
 function draw () {
