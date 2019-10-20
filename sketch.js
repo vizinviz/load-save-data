b07dd1be273f88f57efe47ba1e22260006f8ac74
chrischne <chrischne@gmail.com>
2019-10-20 15:52:59 +0200

    loadData

--- sketch.js
+++ sketch.js
@@ -1,8 +1,11 @@
 /// <reference path="./libraries/p5.global-mode.d.ts" />
 
+var data = [];
+async function setup () {
+	createCanvas(800, 600);
 
-function setup () {
-	createCanvas(4 * 800, 600);
+	data = await loadData('temperatur.csv');
+	console.log(data);
 
 	frameRate(30);
 
